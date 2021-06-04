d45da0dd3314d85ebe6a6dad4efb8ca9947a1ce6
DivijAbbi <divijabbi@gmail.com>
2021-06-03 20:39:56 -0400

    I added a box

--- sketch.js
+++ sketch.js
@@ -26,6 +26,7 @@ function setup(){
 
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
+    box8 = new Box(100,100,100,100)
     pig3 = new Pig(810, 220);
 
     log3 =  new Log(810,180,300, PI/2);
@@ -56,6 +57,7 @@ function draw(){
     log3.display();
 
     box5.display();
+    box8.display();
     log4.display();
     log5.display();
 
